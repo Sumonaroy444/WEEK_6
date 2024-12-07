3b74e3a7bc62ddd0909899119762de6b5a4c3ec6
Sumonaroy444 <154045972+Sumonaroy444@users.noreply.github.com>
2024-12-07 22:17:17 +0600

    Add files via upload
    update week6

---
+++ B_Mark_the_Dust_Sweeper.cpp
@@ -0,0 +1,41 @@
+
+#include<bits/stdc++.h>
+ #define ll long long int
+ using namespace std;
+ int main(){
+ int t;                  
+ cin>>t;
+    while(t--){
+        int n;              
+        cin>>n;
+        vector<int> a(n);
+ 
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+ 
+        reverse(a.begin(),a.end());
+ 
+        while(!a.empty() and a.back()==0){
+        a.pop_back();
+        }
+ 
+        if(a.empty()){
+            cout<<0<<endl;
+            continue;
+        }
+        ll ans=0;
+        reverse(a.begin(),a.end());
+        n=a.size();
+        for(int i=0;i<n-1;i++){
+            if(a[i]==0){
+                ans++;
+            }
+            else{
+                ans+=a[i];
+            }
+        }
+        cout<<ans<<endl;
+    }
+    
+}
